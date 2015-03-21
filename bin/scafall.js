5e9ca7f3cd4221c175d94cde43752cca986861dd
Graham Whelan <gawhelan@gmail.com>
2015-03-21 23:44:11 +0000

    Added install/uninstall commands

--- bin/scafall.js
+++ bin/scafall.js
@@ -2,9 +2,20 @@
 
 var app = require('commander');
 
-app
-    .version(require('../package.json').version)
+app.version(require('../package.json').version)
     .command('init <template>', 'Initialize a new project.')
     .command('ls', 'List all installed generators.')
-    .parse(process.argv)
 ;
+
+app.command('install <generator>')
+    .alias('i')
+    .description('Install a generator.')
+    .action(require('../lib/commands/install'));
+
+
+app.command('uninstall <generator>')
+    .alias('rm')
+    .description('Uninstall a generator.')
+    .action(require('../lib/commands/uninstall'));
+
+app.parse(process.argv);
