5c2bd89b7d6c6d5a613feea992afbffac844a719
Graham Whelan <gawhelan@gmail.com>
2015-03-21 22:25:13 +0000

    Add command to list installed generators

--- bin/scafall.js
+++ bin/scafall.js
@@ -5,5 +5,6 @@ var app = require('commander');
 app
     .version(require('../package.json').version)
     .command('init <template>', 'Initialize a new project.')
+    .command('ls', 'List all installed generators.')
     .parse(process.argv)
 ;
